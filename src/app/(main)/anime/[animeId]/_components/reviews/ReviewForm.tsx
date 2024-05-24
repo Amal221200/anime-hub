1ea307fec0ef30e664bc6ae5ae99c82a7049ad03
Amal221200 <amalmurikkoli2000@gmail.com>
2024-05-24 07:59:35 +0530

    review form

--- src/app/(main)/anime/[animeId]/_components/reviews/ReviewForm.tsx
+++ src/app/(main)/anime/[animeId]/_components/reviews/ReviewForm.tsx
@@ -5,10 +5,10 @@ import { FormEvent } from "react"
 const ReviewForm = ({ handleSubmit, isLoading }: { handleSubmit: (e: FormEvent) => void, isLoading?: boolean }) => {
 
     return (
-        <form className="my-4 flex items-center gap-2" onSubmit={handleSubmit}>
-            <textarea name="review" id="review" placeholder="Add a review" className="flex-1 resize-none rounded-md bg-zinc-800 px-2 py-1 outline-none" required />
+        <form className="my-4 space-y-2" onSubmit={handleSubmit}>
+            <textarea name="review" id="review" placeholder="Add a review" className="block w-full resize-none rounded-md bg-zinc-800 px-2 py-1 outline-none" required rows={4} />
             <button disabled={isLoading} type="submit"
-                className="rounded-md bg-yellow-950 px-2 py-1 text-white disabled:cursor-not-allowed disabled:opacity-70">
+                className="mr-auto block w-full rounded-md bg-yellow-950 px-3 py-1 text-base text-white disabled:cursor-not-allowed disabled:opacity-70 sm:w-min sm:text-lg">
                 Submit
             </button>
         </form>
