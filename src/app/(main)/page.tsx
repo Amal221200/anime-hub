66a8129798ace7e5cd09a4f5ed193282144f1686
Amal221200 <amalmurikkoli2000@gmail.com>
2024-05-31 14:31:46 +0530

    home page fix

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -8,7 +8,7 @@ import BlogCard from "./_components/blog/BlogCard";
 
 const HomeAnimeSection = dynamic(() => import('./_components/anime/HomeAnimeSection'), { loading: () => <SkeletonSpinner className="h-[50vh]" /> })
 
-const HomeBlogSection = dynamic(() => import('./_components/anime/HomeAnimeSection'), { loading: () => <SkeletonSpinner className="h-[50vh]" /> })
+const HomeBlogSection = dynamic(() => import('./_components/blog/HomeBlogSection'), { loading: () => <SkeletonSpinner className="h-[50vh]" /> })
 
 export default async function Home() {
   const { animes } = await getAnimes({ query: '', page: 1, totalAnimes: 12 })
@@ -17,24 +17,26 @@ export default async function Home() {
   return (
     <div className="min-h-[calc(100vh-160px)] sm:min-h-[calc(100vh-120px)]">
       <Intro />
-        <HomeAnimeSection>
-          {
-            animes?.length ? (
+      <HomeAnimeSection>
+        {
+          !animes ? <h1 className="text-center">{"Couldn't"} fetch anime</h1> :
+            animes.length ? (
               animes.map(anime => (
                 <AnimeCard key={anime.id} anime={anime} home />
               ))
-            ) : <h1 className="text-center">{"Couldn't"} fetch anime</h1>
-          }
-        </HomeAnimeSection>
-        <HomeBlogSection>
-          {
-            blogs?.length ? (
+            ) : <h1 className="text-center">No animes yet</h1>
+        }
+      </HomeAnimeSection>
+      <HomeBlogSection>
+        {
+          !blogs ? <h1 className="text-center">{"Couldn't"} fetch blogs</h1> :
+            blogs.length ? (
               blogs.map(blog => (
                 <BlogCard key={blog.id} blog={blog} home />
               ))
-            ) : <h1 className="text-center">{"Couldn't"} fetch anime</h1>
-          }
-        </HomeBlogSection>
+            ) : <h1 className="text-center">No blogs yet</h1>
+        }
+      </HomeBlogSection>
     </div>
   );
 }
