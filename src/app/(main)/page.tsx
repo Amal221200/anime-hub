32e256bfd40ea464be64e99c0c16ae7117e07b53
Amal221200 <amalmurikkoli2000@gmail.com>
2025-03-01 18:32:00 +0530

    using Suspense in home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,18 +1,20 @@
-import dynamic from "next/dynamic";
 import Intro from "./_components/Intro";
 import HomeSectionLoading from "@/components/loading/HomeSectionLoading";
-
-const HomeAnimeSection = dynamic(() => import('./_components/anime/HomeAnimeSection'), { loading: () => <HomeSectionLoading />, ssr: true })
-
-const HomeBlogSection = dynamic(() => import('./_components/blog/HomeBlogSection'), { loading: () => <HomeSectionLoading />, ssr: true })
+import { Suspense } from "react";
+import HomeAnimeSection from "./_components/anime/HomeAnimeSection";
+import HomeBlogSection from "./_components/blog/HomeBlogSection";
 
 export default async function Home() {
 
   return (
     <div className="min-h-[calc(100vh-160px)] sm:min-h-[calc(100vh-120px)]">
       <Intro />
-      <HomeAnimeSection />
-      <HomeBlogSection />
+      <Suspense fallback={<HomeSectionLoading />}>
+        <HomeAnimeSection />
+      </Suspense>
+      <Suspense fallback={<HomeSectionLoading />}>
+        <HomeBlogSection />
+      </Suspense>
     </div>
   );
 }
